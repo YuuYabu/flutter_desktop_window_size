db0f90baade24f60eede036371b9e32be27b8862
Yuu YABU <wongty0220@gmail.com>
2024-11-12 10:18:19 +0900

    `window_manager`を初期化しウィンドウプロパティを指定

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  // 初期化
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // window_managerを初期化
+  windowManager.ensureInitialized();
+  // ウィンドウプロパティを指定
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(1280, 720),
+    center: true,
+  );
+
+  // ウィンドウを表示
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(const MainApp());
 }
 
